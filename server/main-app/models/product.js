7360513dbe4552f41470079fbb5abfd07eec56be
A. Y. Firdaus <amyar32@gmail.com>
2021-03-31 17:21:01 +0700

    menambah comment

--- server/main-app/models/product.js
+++ server/main-app/models/product.js
@@ -1,82 +1,85 @@
-//import untuk membandingkan object id
+//impor untuk nantinya memakai method membandingkan object id
 const mongodb = require("mongodb");
 
 const getDb = require("../util/database").getDb;
 
 class Product {
-    constructor(title, price, description, imageUrl, id) {
-        this.title = title;
-        this.price = price;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this._id = new mongodb.ObjectId(id);
+  constructor(title, price, description, imageUrl, id) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+
+    // apabila ada tidak ada id di controller yang menginisiasi Class ini maka, akan membuat produk baru
+    // dan apabila ada maka akan mengupdate berdasarkan id yang dibawanya
+    this._id = id ? new mongodb.ObjectId(id) : null;
+  }
+
+  save() {
+    const db = getDb();
+    let dbOp;
+
+    if (this._id) {
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
     }
 
-    save() {
-        const db = getDb();
-        let dbOp;
+    //apabila tidak ada collection di server, maka first init akan membuatkannya otomatis
+    return dbOp
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-        if (this._id) {
-            dbOp = db
-                .collection("products")
-                .updateOne({ _id: this._id }, { $set: this });
-        } else {
-            dbOp = db.collection("products").insertOne(this);
-        }
+  static fetchAll() {
+    const db = getDb();
+    //find() tidak mereturn promises
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-        //apabila tidak ada collection di server, maka first init akan membuatkannya otomatis
-        return dbOp
-            .then((result) => {
-                console.log(result);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    static fetchAll() {
-        const db = getDb();
-        //find() tidak mereturn promises
-        return db
-            .collection("products")
-            .find()
-            .toArray()
-            .then((products) => {
-                console.log(products);
-                return products;
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
-
-    static findById(prodId) {
-        const db = getDb();
-        return db
-            .collection("products")
-            .find({ _id: new mongodb.ObjectId(prodId) })
-            .next()
-            .then((product) => {
-                console.log(product);
-                return product;
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
-
-    static deleteById(prodId) {
-        const db = getDb();
-        return db
-            .collection("products")
-            .deleteOne({ _id: new mongodb.ObjectId(prodId) })
-            .then((result) => {
-                console.log("Deleted");
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log("Deleted");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
 
 module.exports = Product;
