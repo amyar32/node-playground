f4565fca051a1abbedcf80b9966dd547d23e3a43
A. Y. Firdaus <amyar32@gmail.com>
2021-04-14 11:31:52 +0700

    add auth

---
+++ 3-authentication/2-app/app.js
@@ -0,0 +1,74 @@
+require("dotenv").config();
+
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+
+//CSURF HERE
+const csrf = require("csurf");
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const app = express();
+const store = new MongoDBStore({
+  uri: process.env.DB_URI,
+  collection: "sessions",
+});
+
+//CSURF
+const csrfProtection = csrf();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(csrfProtection);
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(process.env.DB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then((result) => {
+    console.log("Connected!");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ 3-authentication/2-app/controllers/auth.js
@@ -0,0 +1,71 @@
+const bcrypt = require("bcryptjs");
+const { openDelimiter } = require("ejs");
+const User = require("../models/user");
+
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: false,
+    csrfToken: req.csrfToken(),
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((isMatch) => {
+          if (isMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect("/");
+            });
+          }
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postSignup = async (req, res, next) => {
+  const { email, password, confirmPassword } = req.body;
+  const foundUser = await User.findOne({ email });
+  if (foundUser) {
+    res.redirect("/signup");
+  } else {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await new User({
+      email,
+      password: hashedPassword,
+      confirmPassword,
+    });
+    newUser.save();
+    res.redirect("/login");
+  }
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
